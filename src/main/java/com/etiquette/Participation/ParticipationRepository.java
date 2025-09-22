fb240ff9d3d5aa807bce8c5ab85b4a36b72c73c3
hspacesi34 <jarry.cedric34@gmail.com>
2025-09-22 15:12:58 +0200

    feat: implementer Participation controllet et CRUD

--- src/main/java/com/etiquette/Participation/ParticipationRepository.java
+++ src/main/java/com/etiquette/Participation/ParticipationRepository.java
@@ -1,6 +1,10 @@
 package com.etiquette.Participation;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
+
+import com.etiquette.User.User;
 
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called roleRepository
@@ -8,5 +12,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ParticipationRepository extends CrudRepository<Participation, Integer> {
 
-
+    Optional<Participation> findByUser(User user);
 }
