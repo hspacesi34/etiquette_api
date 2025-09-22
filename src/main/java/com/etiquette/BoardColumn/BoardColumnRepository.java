0ad2e155a0cbc4a9d360cea128906d839cd2dbbe
hspacesi34 <jarry.cedric34@gmail.com>
2025-09-22 11:43:40 +0200

    feat: implementer BoardColumn controller et CRUD

--- src/main/java/com/etiquette/BoardColumn/BoardColumnRepository.java
+++ src/main/java/com/etiquette/BoardColumn/BoardColumnRepository.java
@@ -1,5 +1,7 @@
 package com.etiquette.BoardColumn;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -8,5 +10,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface BoardColumnRepository extends CrudRepository<BoardColumn, Integer> {
 
-
+    Optional<BoardColumn> findByName(String name);
 }
