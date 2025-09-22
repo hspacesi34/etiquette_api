5e07657404d97b663851453b8270bfcafd507bd4
hspacesi34 <jarry.cedric34@gmail.com>
2025-09-22 10:49:17 +0200

    feat: implementer Role controller et CRUD

--- src/main/java/com/etiquette/Role/RoleRepository.java
+++ src/main/java/com/etiquette/Role/RoleRepository.java
@@ -1,5 +1,7 @@
 package com.etiquette.Role;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -8,5 +10,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends CrudRepository<Role, Integer> {
 
-
+    Optional<Role> findByName(String name);
 }
